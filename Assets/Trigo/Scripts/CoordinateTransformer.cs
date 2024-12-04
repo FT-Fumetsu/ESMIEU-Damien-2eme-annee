fe7e2367780713eb5e0a8d777efc944e89c5ba18
FT-Fumetsu <esmieudamien18@gmail.com>
2024-12-04 14:35:26 +0100

    Add Namespaces

--- Assets/Trigo/Scripts/CoordinateTransformer.cs
+++ Assets/Trigo/Scripts/CoordinateTransformer.cs
@@ -1,55 +1,58 @@
 using UnityEngine;
 using UnityEngine.SocialPlatforms;
 
-public class CoordinateTransformer : MonoBehaviour
+namespace Coordinates
 {
-    [SerializeField] private bool _localCoordonatesBool;
-    [SerializeField] private GameObject _gameObjectReference;
-
-
-    private void OnDrawGizmos()
+    public class CoordinateTransformer : MonoBehaviour
     {
-        Vector2 worldPoint = _gameObjectReference.transform.position;
-        Vector2 localCoordonates = WorldToLocal(worldPoint);
+        [SerializeField] private bool _localCoordonatesBool;
+        [SerializeField] private GameObject _gameObjectReference;
 
-        Vector2 worldCoordonates = LocalToWorld(localCoordonates);
 
-        if (_localCoordonatesBool)
+        private void OnDrawGizmos()
         {
-            Debug.Log("Local Coordonates : " + localCoordonates);
+            Vector2 worldPoint = _gameObjectReference.transform.position;
+            Vector2 localCoordinates = WorldToLocal(worldPoint);
+
+            Vector2 worldCoordinates = LocalToWorld(localCoordinates);
+
+            if (_localCoordonatesBool)
+            {
+                Debug.Log("Local Coordonates : " + localCoordinates);
+            }
+            else
+            {
+                Debug.Log("World Coordonates : " + worldCoordinates);
+            }
         }
-        else
+        public Vector2 LocalToWorld(Vector2 localPoint)
         {
-            Debug.Log("World Coordonates : " + worldCoordonates);
+            Vector2 localRight = transform.right;
+            Vector2 localUp = transform.up;
+
+            Vector2 globalOrigin = transform.position;
+
+            Vector2 globalPoint =
+                globalOrigin +
+                localPoint.x * localRight +
+                localPoint.y * localUp;
+
+            return globalPoint;
+        }
+
+        public Vector2 WorldToLocal(Vector2 globalPoint)
+        {
+            Vector2 localRight = transform.right;
+            Vector2 localUp = transform.up;
+
+            Vector2 globalOrigin = transform.position;
+
+            Vector2 offset = globalOrigin - globalPoint;
+
+            float localX = Vector2.Dot(offset, localRight);
+            float localY = Vector2.Dot(offset, localUp);
+
+            return new Vector3(localX, localY);
         }
     }
-    public Vector2 LocalToWorld(Vector2 localPoint)
-    {
-        Vector2 localRight = transform.right;
-        Vector2 localUp = transform.up;
-
-        Vector2 globalOrigin = transform.position;
-
-        Vector2 globalPoint =
-            globalOrigin +
-            localPoint.x * localRight +
-            localPoint.y * localUp;
-
-        return globalPoint;
-    }
-
-    public Vector2 WorldToLocal(Vector2 globalPoint)
-    {
-        Vector2 localRight = transform.right;
-        Vector2 localUp = transform.up;
-
-        Vector2 globalOrigin = transform.position;
-
-        Vector2 offset = globalOrigin - globalPoint;
-
-        float localX = Vector2.Dot(offset, localRight);
-        float localY = Vector2.Dot(offset, localUp);
-
-        return new Vector3(localX, localY);
-    }
-}   
+}
--- Assets/Trigo/Scripts/SwitchColorOfGizmo.cs
+++ Assets/Trigo/Scripts/SwitchColorOfGizmo.cs
@@ -1,94 +1,97 @@
 using UnityEngine;
 
-public class SwitchColorOfGizmo : MonoBehaviour
+namespace Sphere
 {
-    [SerializeField] private Color _color;
-    [SerializeField] private Color _startingColor;
-    [SerializeField] private Color _pointColor;
-    [SerializeField] private Color _gizmoColor;
-    [SerializeField] private Color _hollowPurple;
-    [SerializeField] private Transform _otherPoint;
-    [SerializeField] private SwitchColorOfGizmo _otherCircle;
-    [SerializeField] private Transform _otherCenter;
-    [SerializeField, Range (0, 10)] public float _circleRadius;
-
-    public float Radius => _circleRadius;
-
-    private void DrawCircle()
+    public class SwitchColorOfGizmo : MonoBehaviour
     {
-        Gizmos.color = _color;  
-        Gizmos.DrawSphere(transform.position, _circleRadius);
+        [SerializeField] private Color _color;
+        [SerializeField] private Color _startingColor;
+        [SerializeField] private Color _pointColor;
+        [SerializeField] private Color _gizmoColor;
+        [SerializeField] private Color _hollowPurple;
+        [SerializeField] private Transform _otherPoint;
+        [SerializeField] private SwitchColorOfGizmo _otherCircle;
+        [SerializeField] private Transform _otherCenter;
+        [SerializeField, Range(0, 10)] public float _circleRadius;
 
-        ChangeColorOnCollidePoint();
+        public float Radius => _circleRadius;
 
-        ChangeColorOnCollideGizmo();
-    }
-
-    private void OnDrawGizmos()
-    {
-        DrawCircle();
-    }
-
-    private void ChangeColorOnCollidePoint()
-    {
-        Vector3 vectorCenterOtherPoint = _otherPoint.position - transform.position;
-        float sqrVectorCenterPoint = vectorCenterOtherPoint.sqrMagnitude;
-        if (sqrVectorCenterPoint <= _circleRadius * _circleRadius)
+        private void DrawCircle()
         {
-            _color = _pointColor;
+            Gizmos.color = _color;
+            Gizmos.DrawSphere(transform.position, _circleRadius);
+
+            ChangeColorOnCollidePoint();
+
+            ChangeColorOnCollideGizmo();
         }
-        else if (sqrVectorCenterPoint > _circleRadius * _circleRadius)
+
+        private void OnDrawGizmos()
         {
-            _color = _startingColor;
+            DrawCircle();
         }
+
+        private void ChangeColorOnCollidePoint()
+        {
+            Vector3 vectorCenterOtherPoint = _otherPoint.position - transform.position;
+            float sqrVectorCenterPoint = vectorCenterOtherPoint.sqrMagnitude;
+            if (sqrVectorCenterPoint <= _circleRadius * _circleRadius)
+            {
+                _color = _pointColor;
+            }
+            else if (sqrVectorCenterPoint > _circleRadius * _circleRadius)
+            {
+                _color = _startingColor;
+            }
+        }
+
+        private void ChangeColorOnCollideGizmo()
+        {
+            Vector3 vectorCenterOtherGizmo = _otherCenter.position - transform.position;
+            float sqrVectorCenterGizmo = vectorCenterOtherGizmo.sqrMagnitude;
+            if (sqrVectorCenterGizmo <= (_circleRadius + _otherCircle.Radius) * (_circleRadius + _otherCircle.Radius))
+            {
+                _color = _gizmoColor;
+            }
+            else if (sqrVectorCenterGizmo > (_circleRadius + _otherCircle.Radius) * (_circleRadius + _otherCircle.Radius))
+            {
+                _color = _startingColor;
+            }
+        }
+
+        //private void ChangeColorOnCollidePointMathf()
+        //{
+        //    Vector3 vectorCenterOtherPoint = _otherPoint.position - transform.position;
+        //    float sqrVectorCenterPoint = Mathf.Sqrt((vectorCenterOtherPoint.x * vectorCenterOtherPoint.x) + (vectorCenterOtherPoint.y * vectorCenterOtherPoint.y));
+        //    Debug.Log(sqrVectorCenterPoint + " + " + _circleRadius);
+
+        //    if (sqrVectorCenterPoint <= _circleRadius)
+        //    {
+        //        _color = _pointColor;
+        //    }
+
+        //    else if (sqrVectorCenterPoint > _circleRadius)
+        //    {
+        //        _color = _startingColor;
+        //    }
+        //}
+
+        //private void ChangeColorOnCollideGizmoMathf()
+        //{
+        //    Vector3 vectorCenterOtherGizmo = _otherCenter.position - transform.position;
+        //    float sqrVectorCenterGizmo = Mathf.Sqrt((vectorCenterOtherGizmo.x * vectorCenterOtherGizmo.x) + (vectorCenterOtherGizmo.y * vectorCenterOtherGizmo.y));
+
+        //    //Debug.Log(_circleRadius + _otherCircle.Radius);
+
+        //    if (sqrVectorCenterGizmo <= _circleRadius + _otherCircle.Radius)
+        //    {
+        //        _color = _gizmoColor;
+        //    }
+
+        //    else if (sqrVectorCenterGizmo > _circleRadius + _otherCircle.Radius)
+        //    {
+        //        _color = _startingColor;
+        //    }
+        //}
     }
-
-    private void ChangeColorOnCollideGizmo()
-    {
-        Vector3 vectorCenterOtherGizmo = _otherCenter.position - transform.position;
-        float sqrVectorCenterGizmo = vectorCenterOtherGizmo.sqrMagnitude;
-        if (sqrVectorCenterGizmo <= (_circleRadius + _otherCircle.Radius) * (_circleRadius + _otherCircle.Radius))
-        {
-            _color = _gizmoColor;
-        }
-        else if (sqrVectorCenterGizmo > (_circleRadius + _otherCircle.Radius) * (_circleRadius + _otherCircle.Radius))
-        {
-            _color = _startingColor;
-        }
-    }
-
-    //private void ChangeColorOnCollidePointMathf()
-    //{
-    //    Vector3 vectorCenterOtherPoint = _otherPoint.position - transform.position;
-    //    float sqrVectorCenterPoint = Mathf.Sqrt((vectorCenterOtherPoint.x * vectorCenterOtherPoint.x) + (vectorCenterOtherPoint.y * vectorCenterOtherPoint.y));
-    //    Debug.Log(sqrVectorCenterPoint + " + " + _circleRadius);
-
-    //    if (sqrVectorCenterPoint <= _circleRadius)
-    //    {
-    //        _color = _pointColor;
-    //    }
-
-    //    else if (sqrVectorCenterPoint > _circleRadius)
-    //    {
-    //        _color = _startingColor;
-    //    }
-    //}
-
-    //private void ChangeColorOnCollideGizmoMathf()
-    //{
-    //    Vector3 vectorCenterOtherGizmo = _otherCenter.position - transform.position;
-    //    float sqrVectorCenterGizmo = Mathf.Sqrt((vectorCenterOtherGizmo.x * vectorCenterOtherGizmo.x) + (vectorCenterOtherGizmo.y * vectorCenterOtherGizmo.y));
-
-    //    //Debug.Log(_circleRadius + _otherCircle.Radius);
-
-    //    if (sqrVectorCenterGizmo <= _circleRadius + _otherCircle.Radius)
-    //    {
-    //        _color = _gizmoColor;
-    //    }
-
-    //    else if (sqrVectorCenterGizmo > _circleRadius + _otherCircle.Radius)
-    //    {
-    //        _color = _startingColor;
-    //    }
-    //}
 }
